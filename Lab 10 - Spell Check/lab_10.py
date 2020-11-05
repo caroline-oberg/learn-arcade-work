32e29bc98b3800bf9ef0cfb8daea0e368f3c2fbd
Caroline Oberg <caroline.oberg@my.simpson.edu>
2020-11-04 21:46:21 -0600

    Resubmission on lab 10

--- Lab 10 - Spell Check/lab_10.py
+++ Lab 10 - Spell Check/lab_10.py
@@ -5,55 +5,71 @@ def split_line(line):
 
 def main():
 
-    my_file = open("AliceInWonderLand200.txt")
+    my_file = open("dictionary.txt")
 
-    name_list = []
+    dictionary_list = []
 
     for line in my_file:
 
         line = line.strip()
 
-        name_list.append(line)
+        dictionary_list.append(line)
 
     my_file.close()
 
-    print( "There were", len(name_list), "names in the file.")
-
     # --- Linear search
-    key = "Alice In Wonderland"
+    print("Linear Search")
+    my_file = open("AliceInWonderLand200.txt")
 
-    current_list_position = 0
+    line_number = 0
 
-    while current_list_position < len(name_list) and name_list[current_list_position] != key:
-        current_list_position += 1
+    for line in my_file:
+        line_number += 1
+        word_list = split_line(line)
 
-    if current_list_position < len(name_list):
-        print("The name is at position", current_list_position)
-    else:
-        print("The name was not in the list.")
+        for word in word_list:
+            key = word.upper()
 
-    # --- Binary search
-    key = "Alice In Wonderland"
-    lower_bound = 0
-    upper_bound = len(name_list)-1
-    found = False
+            current_list_position = 0
 
-    while lower_bound <= upper_bound and not found:
+            while current_list_position < len(dictionary_list) and dictionary_list[current_list_position] != key:
+                current_list_position += 1
 
+            if current_list_position == len(dictionary_list):
+                print("Line", line_number, "The word misspelled is", word)
 
-        middle_pos = (lower_bound + upper_bound) // 2
-
-        if name_list[middle_pos] < key:
-            lower_bound = middle_pos + 1
-        elif name_list[middle_pos] > key:
-            upper_bound = middle_pos - 1
-        else:
-            found = True
-
-    if found:
-        print( "The name is at position", middle_pos)
-    else:
-        print( "The name was not in the list." )
+    my_file.close()
 
 
+    print("Binary Search")
+    my_file = open("AliceInWonderLand200.txt")
+
+    line_number = 0
+
+    for line in my_file:
+        line_number += 1
+        word_list = split_line(line)
+
+        for word in word_list:
+            key = word.upper()
+            lower_bound = 0
+            upper_bound = len(dictionary_list)-1
+            found = False
+
+            while lower_bound <= upper_bound and not found:
+
+
+                middle_pos = (lower_bound + upper_bound) // 2
+
+                if dictionary_list[middle_pos] < key:
+                    lower_bound = middle_pos + 1
+                elif dictionary_list[middle_pos] > key:
+                    upper_bound = middle_pos - 1
+                else:
+                    found = True
+
+            if not found:
+                print("Line", line_number, "The word misspelled is", word)
+
+    my_file.close()
 main()
